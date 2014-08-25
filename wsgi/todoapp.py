c9d5ab30cc853839aabc48d9e17133c5bab6b8a0
Wim Wauters <info@wapptastic.com>
2014-08-25 14:45:32 +0200

    Added functionality

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -1,8 +1,8 @@
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, render_template, abort
- 
+  
 app = Flask(__name__)
  
 app.config.from_pyfile('todo.cfg')
@@ -11,23 +11,26 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column('todo_id', db.Integer, primary_key=True)
-    content = db.Column(db.String(60))
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
     done = db.Column(db.Boolean)
-    created_at = db.Column(db.DateTime)
+    pub_date = db.Column(db.DateTime)
  
-    def __init__(self, content, done):
-        self.content = content
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
         self.done = False
-        self.created_at = datetime.utcnow()
+        self.pub_date = datetime.utcnow()
  
+
 @app.route('/new', methods=['GET', 'POST'])
 def new():
     if request.method == 'POST':
-            todo = Todo(request.form['content'])
+            todo = Todo(request.form['title'], request.form['text'])
             db.session.add(todo)
             db.session.commit()
             return redirect(url_for('index'))
-    return render_template('new.html') 
- 
+    return render_template('new.html')
+
 if __name__ == '__main__':
     app.run()
