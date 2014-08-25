8ee9a5428437dcf18ddcb80d2637123dc1deb0ea
Wim Wauters <info@wapptastic.com>
2014-08-25 15:17:48 +0200

    Added functionality

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -11,22 +11,20 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column('todo_id', db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    text = db.Column(db.String)
+    content = db.Column(db.String(60))
     done = db.Column(db.Boolean)
-    pub_date = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime)
  
-    def __init__(self, title, text):
-        self.title = title
-        self.text = text
+    def __init__(self, content):
+        self.content = content
         self.done = False
-        self.pub_date = datetime.utcnow()
+        self.created_at = datetime.utcnow()
  
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
     if request.method == 'POST':
-            todo = Todo(request.form['title'], request.form['text'])
+            todo = Todo(request.form['content'])
             db.session.add(todo)
             db.session.commit()
             return redirect(url_for('index'))
