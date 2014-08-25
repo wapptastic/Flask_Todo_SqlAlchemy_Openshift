0b22faeb0c118ed758c6a60920d64f5704d0d1be
Wim Wauters <info@wapptastic.com>
2014-08-25 15:25:11 +0200

    Added functionality

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -27,7 +27,7 @@ def new():
             todo = Todo(request.form['content'])
             db.session.add(todo)
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect('/')
     return render_template('new.html')
 
 if __name__ == '__main__':
